394166ea14b243ba69407a6330f50a3cc5860122
Roland Groen <roland@edia.nl>
2020-04-06 15:51:16 +0200

    NOJIRA - Make use of ngrok
     - Simplified script files.
     - Make use of ngrok to be public accessible.

--- entrypoint.bash
+++ entrypoint.bash
@@ -3,45 +3,18 @@
 # see https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 
-function check_variable {
-  eval "VAL=\"\$$1\""
-  if [ -z "$VAL" ]; then
-    echo "The environment variable $1 is required."
-    exit 1
-  fi
-}
-
-check_variable "HOST_URL"
-check_variable "JWT_ISSUER"
-check_variable "CLIENT_KEY"
-
-## Check if there is a private key file added to the container and set in JWT_PRIVATE_KEY_FILE.
-if [ ! -s "$JWT_PRIVATE_KEY_FILE" ]; then
-  ## Check if a value is given in JWT_PRIVATE_KEY.
-  if [ -z "$JWT_PRIVATE_KEY" ]; then
-    echo "Generating keypair becasue envorionment variable JWT_PRIVATE_KEY environment variable is not set"
-    bash /tools/keygen.sh "$JWT_PUBLIC_KEY_FILE" "$JWT_PRIVATE_KEY_FILE"
-  else
-    echo "Using keys from JWT_PUBLIC_KEY and JWT_PRIVATE_KEY because JWT_PRIVATE_KEY is set (the public key is not required)"
-    if [ -n "$JWT_PUBLIC_KEY" ]; then
-      echo "$JWT_PUBLIC_KEY" >"$JWT_PUBLIC_KEY_FILE"
-    fi
-    echo "$JWT_PRIVATE_KEY" >"$JWT_PRIVATE_KEY_FILE"
-  fi
-fi
+echo "HOST_URL is ${HOST_URL}"
+echo "JWT_ISSUER is ${JWT_ISSUER}"
+echo "CLIENT_KEY is ${CLIENT_KEY}"
 
 if [ -s "$JWT_PUBLIC_KEY_FILE" ]; then
   # Note that the public key file is only used for logging here, the server does not need one.
   echo "The public key is set to:"
   cat "$JWT_PUBLIC_KEY_FILE"
   echo ""
-fi
-
-if [ -z "$CLIENT_SECRET" ]; then
-  echo "Generating client secret becasue CLIENT_SECRET environment variable is not set:"
-  export CLIENT_SECRET="$(openssl rand -hex 32)"
-  echo "$CLIENT_SECRET"
-  echo ""
+else
+  echo "The public key file ${JWT_PUBLIC_KEY_FILE} is not found."
+  exit 1
 fi
 
 # Use envsubst to dereference the variables
@@ -60,7 +33,7 @@ if [ -n "$SCHEMES" ]; then
   IFS=$' '
   for SCHEME in $SCHEMES; do
     echo "Downloading scheme $SCHEME"
-    /usr/local/bin/irma scheme download /configuration/schemes "$SCHEME"
+    /usr/local/bin/irma scheme download /configuration/schemes "$SCHEME" || true
   done
 fi
 
